44f6ef2636c1afc58c7e943a7e79f8490d0d5e92
Rahul Sehgal <47705227+rahul9810@users.noreply.github.com>
2019-03-30 08:48:09 +0000

    tulip: prop: Add back volte property
    got deleted on commit https://github.com/rama982/android_device_xiaomi_tulip/commit/fd48b8638442714bf82084d7711fd3ebbff14859
    
    Change-Id: I80eac94990440918cafdf81a551daf43b4b8d222

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -250,6 +250,17 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.timed.enable=true
 
+# Volte
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    persist.dbg.volte_avail_ovr=1  \
+    persist.dbg.vt_avail_ovr=1  \
+    persist.dbg.wfc_avail_ovr=1  \
+    persist.dbg.ims_volte_enable=1  \
+    persist.radio.data_con_rprt=1  \
+    persist.radio.calls.on.ims=1  \
+    persist.radio.rat_on=combine  \
+    persist.radio.data_ltd_sys_ind=1
+
 # WFD display
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.debug.wfd.enable=1  \
