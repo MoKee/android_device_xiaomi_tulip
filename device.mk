c72b1add1562b4037769a364731b0628f732de38
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-10 14:55:27 +0800

    tulip device: Remove unused or duplicated packages
    Change-Id: Ibe9ebfb1fdacafb13f0fd544ff33f382ffc8e0cd

--- device.mk
+++ device.mk
@@ -194,7 +194,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
-    fingerprintd \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_tulip
 
 PRODUCT_COPY_FILES += \
@@ -225,8 +224,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-service-qti \
     libgnss \
     libgnsspps \
-    libsensorndkbridge \
-    libvehiclenetwork-native
+    libsensorndkbridge
 
 # Health
 PRODUCT_PACKAGES += \
@@ -436,9 +434,7 @@ PRODUCT_PACKAGES += \
     libQWiFiSoftApCfg \
     libwifi-hal-qcom \
     libwpa_client \
-    vendor.qti.hardware.wifi.supplicant@1.0_vendor \
     wificond \
-    wifilogd \
     wpa_supplicant \
     wpa_supplicant.conf
 
